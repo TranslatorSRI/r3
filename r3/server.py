6cb63117d36c859c1df44207df80129b8871fbac
Phil Owen <phillipsowen@outlook.com>
2020-07-23 09:18:57 -0400

    adding some error traps

--- r3/server.py
+++ r3/server.py
@@ -41,25 +41,29 @@ async def get_normalized_node_handler(request):
 
     Use GET for single key, MGET for multiple.
     """
-    if isinstance(request.args['curie'], list):
-        references = await app.redis_connection0.mget(*request.args['curie'], encoding='utf-8')
-        references_nonnan = [reference for reference in references if reference is not None]
-        if not references_nonnan:
-            return response.text('No matches found for the specified curie(s).', status=404)
-        values = await app.redis_connection1.mget(*references_nonnan, encoding='utf-8')
-        values = [json.loads(value) if value is not None else None for value in values]
-        dereference = dict(zip(references_nonnan, values))
-        return response.json({
-            key: dereference[reference] if reference is not None else None
-            for key, reference in zip(request.args['curie'], references)
-        })
+    # was an arg passed in
+    if len(request.args) != 0:
+        if isinstance(request.args['curie'], list):
+            references = await app.redis_connection0.mget(*request.args['curie'], encoding='utf-8')
+            references_nonnan = [reference for reference in references if reference is not None]
+            if not references_nonnan:
+                return response.text('No matches found for the specified curie(s).', status=404)
+            values = await app.redis_connection1.mget(*references_nonnan, encoding='utf-8')
+            values = [json.loads(value) if value is not None else None for value in values]
+            dereference = dict(zip(references_nonnan, values))
+            return response.json({
+                key: dereference[reference] if reference is not None else None
+                for key, reference in zip(request.args['curie'], references)
+            })
+        else:
+            reference = await app.redis_connection0.get(request.args['curie'], encoding='utf-8')
+            if reference is None:
+                return response.json({request.args['curie']: None})
+            value = await app.redis_connection1.get(reference, encoding='utf-8')
+            value = json.loads(value) if value is not None else None
+            return response.json({request.args['curie']: value})
     else:
-        reference = await app.redis_connection0.get(request.args['curie'], encoding='utf-8')
-        if reference is None:
-            return response.json({request.args['curie']: None})
-        value = await app.redis_connection1.get(reference, encoding='utf-8')
-        value = json.loads(value) if value is not None else None
-        return response.json({request.args['curie']: value})
+        return response.text('Error parsing query string.', status=500)
 
 @app.route('/get_semantic_types')
 async def get_semantic_types_handler(request):
@@ -83,31 +87,34 @@ async def get_curie_prefixes_handler(request):
 
     # was an arg passed in
     if len(request.args) != 0:
-        # is the input a list
-        if isinstance(request.args['semantic_type'], list):
-            for item in request.args['semantic_type']:
+        try:
+            # is the input a list
+            if isinstance(request.args['semantic_type'], list):
+                for item in request.args['semantic_type']:
+                    # get the curies for this type
+                    curies = await app.redis_connection2.get(item, encoding='utf-8')
+
+                    # did we get any data
+                    if not curies:
+                        curies = '{' + f'"{item}"' + ': "Not Found"}'
+
+                    curies = json.loads(curies)
+
+                    # set the return data
+                    ret_val[item] = {'curie_prefix': [curies]}
+            # else it must be a singleton+6+9++
+            else:
                 # get the curies for this type
-                curies = await app.redis_connection2.get(item, encoding='utf-8')
+                curies = await app.redis_connection2.get(request.args["semantic_type"], encoding='utf-8')
 
                 # did we get any data
                 if not curies:
-                    curies = '{' + f'"{item}"' + ': "Not Found"}'
-
-                curies = json.loads(curies)
+                    return response.text(f'No curie discovered for {request.args["semantic_type"]}.', status=404)
 
                 # set the return data
-                ret_val[item] = {'curie_prefix': [curies]}
-        # else it must be a singleton+6+9++
-        else:
-            # get the curies for this type
-            curies = await app.redis_connection2.get(request.args["semantic_type"], encoding='utf-8')
-
-            # did we get any data
-            if not curies:
-                return response.text(f'No curie discovered for {request.args["semantic_type"]}.', status=404)
-
-            # set the return data
-            ret_val[request.args['semantic_type']] = {'curie_prefix': [curies]}
+                ret_val[request.args['semantic_type']] = {'curie_prefix': [curies]}
+        except Exception:
+            return response.text('Error parsing query string.', status=500)
     else:
         types = await app.redis_connection2.lrange('semantic_types', 0, -1, encoding='utf-8')
 
@@ -124,7 +131,7 @@ async def get_curie_prefixes_handler(request):
             # set the return data
             ret_val[item] = {'curie_prefix': [curies]}
 
-    # return the data to the caller
+# return the data to the caller
     return response.json(ret_val)
 
 app.register_listener(startup_connections, 'after_server_start')
